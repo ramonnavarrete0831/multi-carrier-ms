52daaac5c02b3d9ac3c1435543f9329429fbb75c
Pasteleritos Merida <pasteleritosmerida@MacBook-Pro-de-Pasteleritos.local>
2022-03-03 20:41:02 -0600

    se añade la configuración para mesajes entre gateway y microservicio

--- src/greeting/greeting.controller.ts
+++ src/greeting/greeting.controller.ts
@@ -1,11 +1,12 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
+import { CarrierSMG } from 'src/common/constant';
 import { GreetingService } from './greeting.service';
 
 @Controller()
 export class GreetingController {
     constructor(private greetingService:GreetingService){}
-    @MessagePattern('hello')
+    @MessagePattern({cmd: CarrierSMG.HELLO})
     hello() : string {
        return this.greetingService.hello();
     }
