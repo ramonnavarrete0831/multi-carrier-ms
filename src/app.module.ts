f75cf664a0dfebc2a64b944932c7b73e7094019b
Pasteleritos Merida <pasteleritosmerida@MacBook-Pro-de-Pasteleritos.local>
2022-03-04 11:44:10 -0600

    Se añade tarea cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { GreetingModule } from './greeting/greeting.module';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       envFilePath: ['./.env.develop'],
       isGlobal: true,
